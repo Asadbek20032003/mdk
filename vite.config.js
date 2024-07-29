e47a4046f8435e8eb1f1d3894f11932c90eaec14
Asadbek20032003 <asadbeksolijonov35@gmail.com>
2024-07-30 01:04:39 +0500

    your commit message

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,11 @@ export default defineConfig({
       external: ["@fortawesome/fontawesome-svg-core"],
     },
   },
+  resolve: {
+    alias: {
+      "@fortawesome/fontawesome-svg-core": "node_modules/@fortawesome/fontawesome-svg-core",
+      "@fortawesome/free-solid-svg-icons": "node_modules/@fortawesome/free-solid-svg-icons",
+      "@fortawesome/react-fontawesome": "node_modules/@fortawesome/react-fontawesome",
+    },
+  },
 });
